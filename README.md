3f43e115c94f7c6e9182a8e196d7be8c2c2ec257
Pablo Matias <pmpidone@yahoo.com.ar>
2024-09-23 20:24:37 -0300

    Aplicacion de control de gastos terminada.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# gestiondegastos_inmersionDEV03
+# Gestor de GASTOS
+
+## Ejercicio guiado por los profesores de Alura Latam
+### Inmersion DEV 03
+### Septiembre 2024
+_____________
+
+La funcionalidad básica es poder ir guardando un listado de gastos, un nombre y un monto.
+
+Luego se le agregan mejoras:
+
+**Edición y borrado:** Los datos pueden editarse o borrarse.
+**Se le agrega descripción:** Además del nombre y monto del gasto puede agregarse una descripción.
+**No se puede dar ingreso con valores en 0 o nulos:** Como control de seguridad todos los campos son necesarios.
+**Alerta de gasto:** Si se genera un gasto superior a USD 150 el sistema da una alerta.
+**Unico botón:** Con la posibilidad de manipular el HTML mediante JavaScript se cambia el texto del botón para que el usuario sepa si está editando o generando un dato nuevo.
+
+
+Pablo Matias Pidone
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,43 @@
 let listaNombresGastos = [];
+let listaDescripcionGastos = [];
 let listaValoresGastos = [];
+
+// Variable que se usa como switch de modo ingreso o edicion
+let listaEditar = -1;
 
 function clickBoton() {
   let nomGasto = document.getElementById("nombreGasto").value;
+  let descGasto = document.getElementById("descripcionGasto").value;
   let valGasto = document.getElementById("valorGasto").value;
 
-  if (!nomGasto || valGasto === "" || Number(valGasto) <= 0) {
-    alert("Por favor, ingresa un nombre de gasto válido y un valor mayor a 0.");
-    return; // No continúa si el valor es 0 o nulo
+  // Control de datos, impide datos nulos o 0
+  if (!nomGasto || !descGasto || valGasto === "" || Number(valGasto) <= 0) {
+    alert("Por favor, ingresa un nombre, descripción y valor válidos.");
+    return;
   }
 
-  listaNombresGastos.push(nomGasto);
-  listaValoresGastos.push(valGasto);
+  // Alerta de gastos mayores a USD 150
+  if (Number(valGasto) > 150) {
+    alert("El valor del gasto supera los 150 USD.");
+  }
+  // El if para swichear entre edicion o carga de dato nuevo
+  if (listaEditar === -1) {
 
-  // Limpiar los campos de entrada
+    listaNombresGastos.push(nomGasto);
+    listaDescripcionGastos.push(descGasto);
+    listaValoresGastos.push(parseFloat(valGasto));
+  } else {
+    
+    listaNombresGastos[listaEditar] = nomGasto;
+    listaDescripcionGastos[listaEditar] = descGasto;
+    listaValoresGastos[listaEditar] = parseFloat(valGasto);
+    listaEditar = -1; 
+    document.getElementById("botonFormulario").innerText = "Agregar Gasto";
+  }
+
+  
   document.getElementById("nombreGasto").value = "";
+  document.getElementById("descripcionGasto").value = "";
   document.getElementById("valorGasto").value = "";
 
   actualizarListaGastos();
@@ -28,18 +51,38 @@ function actualizarListaGastos() {
   let totalGastos = 0;
 
   listaNombresGastos.forEach((elemento, posicion) => {
+    const descripcion = listaDescripcionGastos[posicion];
     const valorGasto = listaValoresGastos[posicion];
-    htmlLista += `<li>${elemento} - USD ${valorGasto.toFixed(2)} <button onclick="eliminarGasto(${posicion});">
-            Borrar Gasto
-          </button></li>`;
+
+    htmlLista += `<li>
+      ${elemento} - ${descripcion} - USD ${valorGasto.toFixed(2)} 
+      <button onclick="editarGasto(${posicion});">
+        <i class="fas fa-edit"></i> <!-- Ícono de editar -->
+      </button>
+      <button onclick="eliminarGasto(${posicion});">
+        <i class="fas fa-trash-alt"></i> <!-- Ícono de eliminar -->
+      </button>
+    </li>`;
     totalGastos += Number(valorGasto);
   });
+
   listaElementos.innerHTML = htmlLista;
   totalElementos.innerHTML = totalGastos.toFixed(2);
 }
 
 function eliminarGasto(posicion) {
   listaNombresGastos.splice(posicion, 1);
+  listaDescripcionGastos.splice(posicion, 1);
   listaValoresGastos.splice(posicion, 1);
   actualizarListaGastos();
+}
+
+// La funcion editar se paso de un prompt a los campos del formulario
+function editarGasto(posicion) {
+  document.getElementById("nombreGasto").value = listaNombresGastos[posicion];
+  document.getElementById("descripcionGasto").value = listaDescripcionGastos[posicion];
+  document.getElementById("valorGasto").value = listaValoresGastos[posicion];
+
+  listaEditar = posicion;
+  document.getElementById("botonFormulario").innerText = "Guardar Cambios";
 }
